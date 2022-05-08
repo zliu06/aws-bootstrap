06999679cc1b920099301cb3316f061e3ac51998
Zhi Liu <liuzhi0210@gmail.com>
2022-05-07 22:27:02 -0700

    Add stack name to server output

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = `Hello World from ${hostname()}\n`; 
-const port = 8080; 
-const server = http.createServer((req, res) => { 
+const message = `Hello World from ${hostname()} in ${STACK_NAME}\n`;
+const port = 8080;
+const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end(message);
