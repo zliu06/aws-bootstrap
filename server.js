dd5b513d4e2a46d60781b2ed072fb60df05a5948
Zhi Liu <liuzhi0210@gmail.com>
2022-05-07 20:56:12 -0700

    Print hostname in the message

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = 'Hello Cloud\n'; 
+const message = 'Hello World from ${hostname()}\n'; 
 const port = 8080; 
 const server = http.createServer((req, res) => { 
     res.statusCode = 200;
