51ad9a0ca6cb384886068876a3b8ff9941affc94
Zhi Liu <liuzhi0210@gmail.com>
2022-05-08 17:17:52 -0700

    Use customer domain

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -11,6 +11,9 @@ AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
 
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
+
+DOMAIN=awsbootstrap-920158650541.click
+
 # Generate a personal access token with repo and admin:repo_hook
 #    permissions from https://github.com/settings/tokens
 GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
@@ -60,6 +63,7 @@ aws cloudformation deploy \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
     EC2InstanceType=$EC2_INSTANCE_TYPE \
+    Domain=$DOMAIN \
     GitHubOwner=$GH_OWNER \
     GitHubRepo=$GH_REPO \
     GitHubBranch=$GH_BRANCH \
